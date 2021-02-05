59bf165a1d656eb8a7b940d9e36f7d65caac4e25
Flat Bartender <flat.bartender@gmail.com>
2021-02-05 10:55:57 +0100

    First working version

---
+++ src/main.rs
@@ -0,0 +1,107 @@
+use minimp3::{Frame, Error};
+use cpal::{
+    traits::{HostTrait, DeviceTrait, StreamTrait},
+    Stream,
+};
+use futures_util::{
+    StreamExt,
+};
+use hyper::{
+    body::HttpBody,
+};
+
+use std::sync::{
+    Arc,
+    atomic::{
+        AtomicU8,
+        Ordering,
+    }
+};
+
+#[derive(Debug, Clone)]
+enum PlayerMessage {
+    PlayPause,
+    NeverEnding(()),   // Designed for futures that never stop
+}
+
+#[derive(PartialEq, Eq)]
+enum StreamStatus {
+    Playing,
+    Paused,
+}
+
+struct Player {
+    volume: Arc<AtomicU8>,
+    stream: Stream,
+    stream_status: StreamStatus,
+}
+
+async fn stream_thread(mut tx: ringbuf::Producer<u8>) {
+    let https = hyper_tls::HttpsConnector::new();
+    let client = hyper::client::Client::builder()
+        .build::<_, hyper::Body>(https);
+
+    let mut res = client.get(hyper::Uri::from_static("https://stream.gensokyoradio.net/1/"))
+        .await
+        .expect("Failed to request stream");
+
+    while let Some(chunk) = res.data().await {
+        let slice = &chunk.expect("Failed to convert response body to byte chunk")[..];
+        tx.push_slice(slice);
+    }
+}
+
+async fn decoder_thread(mut tx: ringbuf::Producer<i16>, rx: ringbuf::Consumer<u8>, volume: Arc<AtomicU8>) {
+    let mut decoder = minimp3::Decoder::new(rx);
+
+    loop {
+        match decoder.next_frame() {
+            Ok(Frame {data, ..}) => {
+                let factor = volume.load(Ordering::Relaxed) as f32;
+                let mut iter = data.iter()
+                    .map(|s| (*s as f32 / 100.0 * factor) as i16);
+                tx.push_iter(&mut iter);
+            },
+            Err(Error::Eof) => break,
+            // Do nothing if the ringbuffer is empty because that will happen a lot
+            Err(minimp3::Error::Io(err)) if err.kind() == std::io::ErrorKind::WouldBlock => {},
+            Err(e) => println!("{:?}", e),
+        }
+    }
+}
+
+fn player_init(mut rx: ringbuf::Consumer<i16>) -> Stream {
+    let host = cpal::default_host();
+    let device = host.default_output_device().expect("Failed to acquire default output device");
+    let mut configs = device.supported_output_configs().expect("Failed to list supported output configs");
+    let config = configs.next().expect("Failed to get output config")
+        .with_max_sample_rate().config();
+
+    let stream = device.build_output_stream(&config,
+        move |data: &mut [i16], _: &cpal::OutputCallbackInfo| {
+            data.iter_mut()
+                .for_each(|d| *d = rx.pop().unwrap_or(0i16));
+            },
+            move |err| {
+                println!("{}", err);
+            },
+    ).expect("Failed to create stream");
+
+    stream
+}
+
+#[tokio::main]
+async fn main() {
+    let (stream_tx, stream_rx) = ringbuf::RingBuffer::new(2usize.pow(22)).split();
+    let stream_handle = tokio::spawn(stream_thread(stream_tx));
+
+    let volume = Arc::new(AtomicU8::new(10));
+    let (decoder_tx, decoder_rx) = ringbuf::RingBuffer::new(2usize.pow(22)).split();
+    let decoder_handle = tokio::spawn(decoder_thread(decoder_tx, stream_rx, volume.clone()));
+
+    let stream = player_init(decoder_rx);
+
+    stream.play().expect("Couldn't start playback");
+
+    tokio::try_join!(stream_handle, decoder_handle).expect("Failed to join tasks");
+}
