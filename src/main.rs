7d47378aaf7ea7e1df6aed3b21f5f2f692627f56
flat.bartender@gmail.com <Flat Bartender>
2021-02-13 22:42:56 +0100

    Use windows subsystem in release, console in debug

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,8 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
-#[cfg_attr(build = "release", windows_subsystem = "windows")]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(debug_assertions, windows_subsystem = "console")]
 
 use std::sync::Arc;
 
